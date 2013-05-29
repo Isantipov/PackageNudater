58f84bf31aaaaa1d2771f1531898d44e7383218d
Ivan Antsipau <i.antipov@itransition.com>
2013-05-29 23:10:48 +0300

    extension and core import

---
+++ PackageNudater.Core/VersionChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PackageNudater.Core
+{
+    public class VersionChecker
+    {
+        public IEnumerable<string> GetOutdatedPackages(string packageConfigFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PackageNudater/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Isantipov.PackageNudater
+{
+    static class GuidList
+    {
+        public const string guidPackageNudaterPkgString = "df7211e7-f8c1-4954-8d56-a7c5c3aefcb8";
+        public const string guidPackageNudaterCmdSetString = "b5091c76-881f-437c-8a55-d73c403fc129";
+
+        public static readonly Guid guidPackageNudaterCmdSet = new Guid(guidPackageNudaterCmdSetString);
+    };
+}
